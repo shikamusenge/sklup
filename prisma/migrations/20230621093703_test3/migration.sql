351553e96796cea5aea171200999d1f8f70a45f9
shikamusenge <shikamusenge720@gmail.com>
2023-07-12 02:35:07 +0200

    second commit

--- prisma/migrations/20230621093703_test3/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20230704094004_post_groups_functionality/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "post" ADD COLUMN     "groupsId" INTEGER NOT NULL DEFAULT 1,
-ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'personal';
-
--- AddForeignKey
-ALTER TABLE "post" ADD CONSTRAINT "post_groupsId_fkey" FOREIGN KEY ("groupsId") REFERENCES "groups"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
